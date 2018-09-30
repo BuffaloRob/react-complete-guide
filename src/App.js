f8a6539ff044460b6ba9f125fcd0f65977015891
BuffaloRob <peckham.rob@gmail.com>
2018-09-30 11:53:19 -0400

    added onClick functionality and its handler

--- src/App.js
+++ src/App.js
@@ -10,13 +10,33 @@ class App extends Component {
       { name: 'Chad', age: 44 }
     ]
   }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: newName, age: 33 },
+        { name: 'Chad', age: 55 }
+      ]
+    })
+  }
   render() {
     return <div className="App">
         <h1>I'm React Bitch</h1>
-        <button>Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racecar</Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Robe')}>Switch Name</button>
+      <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}
+        />
+      <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Roberto!')}
+        >My hobbies: Racecar</Person>
+      <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}
+        />
       </div>;
   }
 }
