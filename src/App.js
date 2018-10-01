44e2ec7da52df798bbe57f1c29dc8accf688defc
BuffaloRob <peckham.rob@gmail.com>
2018-10-01 13:51:08 -0400

    added in line styling to 'switch name' button

--- src/App.js
+++ src/App.js
@@ -32,9 +32,18 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return <div className="App">
         <h1>I'm React Bitch</h1>
-        <button onClick={() => this.switchNameHandler('Robe')} value={this.props.name}>Switch Name</button>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Robe')} value={this.props.name}>Switch Name</button>
       <Person 
         name={this.state.persons[0].name} 
         age={this.state.persons[0].age}
