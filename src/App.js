715f36f0abfcd89ea9c6908151a24e02958686ba
BuffaloRob <peckham.rob@gmail.com>
2018-10-01 14:38:59 -0400

    changed way that Person component is conditionally rendered

--- src/App.js
+++ src/App.js
@@ -45,30 +45,36 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     }
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Roberto!')}
+            changeName={this.nameChangeHandler.bind(this)}
+          >My hobbies: Racecar</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </div>
+      )
+    }
+    
     return <div className="App">
         <h1>I'm React Bitch</h1>
         <button 
         style={style}
         onClick={this.togglePersonsHandler}>Bring em out</button>
-        {
-          this.state.showPersons ? 
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}
-              />
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Roberto!')}
-                changeName={this.nameChangeHandler.bind(this)}
-              >My hobbies: Racecar</Person>
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}
-              />
-            </div> : null
-        }
+        
+        {persons}
       </div>;
   }
 }
