3ef2e8a92095699cbd0adc04fb83832066718b0f
BuffaloRob <peckham.rob@gmail.com>
2018-10-09 09:34:28 -0400

    Changed Persons and App to PureComponent & //shouldComponentUpdate

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
   constructor(props) {
     super(props);
     console.log("[Persons.js] inside constructor", props);
@@ -19,12 +19,12 @@ class Persons extends Component {
     console.log('Update [Persons.js] inside componentWillReceiveProps', nextProps);
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[Update Persons.js] inside shouldComponentUpdate', nextProps, nextState);
-    return nextProps.persons !== this.props.persons || 
-      nextProps.changed !== this.props.changed ||
-      nextProps.clicked !== this.props.clicked;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[Update Persons.js] inside shouldComponentUpdate', nextProps, nextState);
+  //   return nextProps.persons !== this.props.persons || 
+  //     nextProps.changed !== this.props.changed ||
+  //     nextProps.clicked !== this.props.clicked;
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log('[Update Persons.js] inside componentWillUpdate', nextProps, nextState);
